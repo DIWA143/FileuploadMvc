f98105059b833db0b0cbf66ebd0eb30dbac120e8
diwakar12 <diwakar.d@sharaaninfo.com>
2024-09-18 16:17:34 +0530

    Add project files.

---
+++ FileuploadMvc/Controllers/LogIn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FileuploadMvc.Models;
+
+namespace FileuploadMvc.Controllers
+{
+    public class LogIn : Controller
+    { 
+        LogIn login = new LogIn();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View(login);
+            // Return a success response; consider returning a token if using JWT.
+           
+        }
+    }
+}
